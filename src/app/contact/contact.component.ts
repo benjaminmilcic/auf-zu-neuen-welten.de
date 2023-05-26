68ab810f50c1bcfaa4b99995568200df49d102b9
Benjamin Milcic <benjamin.milcic@gmail.com>
2023-05-26 14:54:42 +0200

    Send Button change while sending

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,20 +9,27 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
+  isSending = false;
+  sendButtonText = 'Send';
   constructor(private toastr: ToastrService, private http: HttpClient) {}
 
   onSubmit(messageForm: NgForm) {
-    console.log(messageForm.value);
+    this.isSending = true;
+    this.sendButtonText = 'Sending...';
     this.http
       .post('https://nest-form2mail.adaptable.app/', messageForm.value)
       .subscribe(
         () => {
+          this.isSending = false;
+          this.sendButtonText = 'Send';
           messageForm.reset();
           this.toastr.success('Your message was sent!', 'All right!', {
             positionClass: 'toast-bottom-center',
           });
         },
         (error: Error) => {
+          this.isSending = false;
+          this.sendButtonText = 'Send';
           this.toastr.error(
             error.message,
             'An error has occurred, message was not sent!',
